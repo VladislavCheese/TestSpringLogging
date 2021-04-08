4923f0c448d9c64e9bafb5d853c177cf61aba183
Vladislav Syrov <89851502703l@gmail.com>
2021-04-08 13:07:53 +0300

    Initial commit

---
+++ src/main/java/com/example/testspringlogging/TestSpringLoggingApplication.java
@@ -0,0 +1,13 @@
+package com.example.testspringlogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringLoggingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSpringLoggingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testspringlogging/TestSpringLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testspringlogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringLoggingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
